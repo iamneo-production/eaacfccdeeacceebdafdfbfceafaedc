400aaa6b66c329711182a88edb4c85c0a6cf9d18
Iamneo <techadmin@iamneo.ai>
2023-08-25 06:20:13 +0000

    Backup Commit

--- Ashh_Spring_boot/src/test/java/com/example/demo/ToListApplicationTests.java
+++ Ashh_Spring_boot/src/test/java/com/example/demo/ToListApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.examly
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
